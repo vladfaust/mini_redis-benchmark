a1d15de88875dbd0a321d63e3d59e43c9b4d9b58
Vlad Faust <mail@vladfaust.com>
2019-04-15 18:20:48 +0300

    update

--- src/send.cr
+++ src/send.cr
@@ -4,20 +4,15 @@ require "redis"
 
 mini_redis = MiniRedis.new(uri: URI.parse(ENV["REDIS_URL"]))
 redis = Redis.new(url: ENV["REDIS_URL"])
-foo_slice = "foo".to_slice
 
 puts "Begin benchmarking..."
 
 Benchmark.ips do |x|
-  x.report("mini_redis inline") do
-    mini_redis.send("GET foo")
+  x.report("mini_redis") do
+    mini_redis.send("GET", "foo")
   end
 
-  x.report("mini_redis marshalled") do
-    mini_redis.send("GET", foo_slice)
-  end
-
-  x.report("redis marshalled") do
-    redis.get(foo_slice)
+  x.report("crystal-redis") do
+    redis.get("foo")
   end
 end
